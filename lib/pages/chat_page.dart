8bf4d1f23785d497c7be3b86334400924a729258
WalterLimaViana <walter.lima.viana@gmail.com>
2022-07-03 17:05:14 -0300

    Finalizada tela homepage

---
+++ lib/pages/chat_page.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+class ChatPage extends StatefulWidget {
+  final String peerId;
+  final String peerAvatar;
+  final String peerNickname;
+  final String userAvatar;
+
+  const ChatPage(
+      {Key? key,
+      required this.peerNickname,
+      required this.peerAvatar,
+      required this.peerId,
+      required this.userAvatar})
+      : super(key: key);
+
+  @override
+  State<ChatPage> createState() => _ChatPageState();
+}
+
+class _ChatPageState extends State<ChatPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
